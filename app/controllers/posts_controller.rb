dc9ca4fd8fcfe617a893ec42f56080aadcebaf09
AdrianAMJ <adrianmenares22@gmail.com>
2020-03-09 23:38:15 -0300

    Posts prueba

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,50 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
+  end
+
+  def show
+    @Post = Post.find(params[:id])
+  end
+
+  def new
+    @Post = Post.new
+  end
+
+  def create
+    @Post = Post.new(post_params)
+
+    if @Post.save
+      redirect_to @Post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @Post.update(post_params)
+      redirect_to @Post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @Post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_post
+    @Post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
